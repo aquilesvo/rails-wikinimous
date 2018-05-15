a4aba2d3c9f72eee7eeffd49c0b600630b39f837
Aquiles Valdivia <mail@aquiles.de>
2018-05-15 17:07:54 +0200

    articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_134842) do
+ActiveRecord::Schema.define(version: 2018_05_15_140724) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "contect"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
